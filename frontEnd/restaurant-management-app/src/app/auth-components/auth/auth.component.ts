b5c7acdf5fe13892893d1e60e4f3be1146b40931
NinoosMoshi <nalkaton@gmail.com>
2023-10-12 13:44:03 -0500

    mismatch password

--- frontEnd/restaurant-management-app/src/app/auth-components/auth/auth.component.ts
+++ frontEnd/restaurant-management-app/src/app/auth-components/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService } from 'src/app/auth-services/auth.service';
+import { ConfirmPasswordValidator } from 'src/validators/confirm-password.validator';
 
 @Component({
   selector: 'app-auth',
@@ -13,14 +14,14 @@ export class AuthComponent {
   isFormSubmitted:boolean = false;
   validateForm: FormGroup;
 
-  confirmationValidation = (control:FormControl): { [s: string]: boolean} => {
-     if(!control.value){
-        return { requird:true };
-     } else if(control.value !== this.validateForm.controls['password'].value){
-        return { confirm: true, error: true }
-     }
-     return {};
-  }
+  // confirmationValidation = (control:FormControl): { [s: string]: boolean} => {
+  //    if(!control.value){
+  //       return { requird:true };
+  //    } else if(control.value != this.validateForm.controls['password'].value){
+  //       return { confirm: true, error: true }
+  //    }
+  //    return {};
+  // }
 
 
   constructor(private authService: AuthService,
@@ -28,69 +29,55 @@ export class AuthComponent {
 
 
   ngOnInit(){
-    this.validateForm = this.fb.group({
-      name:["",Validators.required],
-      email:["",Validators.required],
-      password:["",Validators.required],
-      checkPassword:["",[Validators.required, this.confirmationValidation]]
-    })
-    //  this.myForm();
+
+     this.myForm();
   }
 
 
 
 
-2
-3
-4
-5
-6
-7
-8
+   myForm(){
+    this.validateForm = this.fb.group({
+      name:["",Validators.required],
+      email:["",Validators.required],
+      password:["",Validators.required],
+      checkPassword:["",[Validators.required]]
+    },
+    {
+      validator: ConfirmPasswordValidator("password", "checkPassword")
+    }
 
-validateFormNew() {
+    );
+   }
 
-        this.isFormSubmitted = true;
-        if (this.validateForm.valid) {
-           // Do the loan calculation
+
+
+
+
+
+
+
+
+
+
+  register(){
+
+    this.isFormSubmitted = true;
+    if (this.validateForm.valid) {
+      console.log(this.validateForm.value)
+      this.authService.signup(this.validateForm.value).subscribe({
+        next: res =>{
+          if(res.id != null){
+            // success message ("SUCCESS", "You're registered successfully")
+          }
+        },
+        error: err =>{
+
         }
-}
+      })
+    }
 
-
-
-
-  //  myForm(){
-  //   this.validateForm = this.fb.group({
-  //     name:["",Validators.required],
-  //     // email:["",Validators.required],
-  //     // password:["",Validators.required],
-  //     // checkPassword:["",[Validators.required, this.confirmationValidation]]
-  //   })
-  //  }
-
-
-
-
-
-
-
-
-
-  // register(){
-
-
-  //   // console.log(this.validateForm.value)
-  //   // this.authService.signup(this.validateForm.value).subscribe({
-  //   //   next: res =>{
-  //   //     if(res.id != null){
-  //   //       // success message ("SUCCESS", "You're registered successfully")
-  //   //     }
-  //   //   },
-  //   //   error: err =>{
-
-  //   //   }
-  //   // })
-  // }
+  }
 
 
 
